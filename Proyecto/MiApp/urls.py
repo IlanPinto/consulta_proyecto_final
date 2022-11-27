e9ef21682e7ba9abbcc0920b98f559b8bb97e44d
IlanPinto <116753638+IlanPinto@users.noreply.github.com>
2022-11-27 18:59:46 +0200

    subida

---
+++ Proyecto/MiApp/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+from MiApp import views
+urlpatterns = [
+    path('', views.inicio, name='Inicio'),
+    path('pages/', views.pages, name='pages'),
+    path('jugadores/', views.mostrar_jugadores_todos, name='jugadores'),
+    path('entrenadores/', views.mostrar_entrenadores_todos, name='entrenadores'),
+    path('torneos/', views.mostrar_torneos_todos, name='torneos'),
+    path('pagina_jugadores/', views.mostrar_jugadores_pagina, name='Jugadores'),
+    path('crear_jugadores/', views.formulario_jugadores, name='FormularioJugadores'),
+    path('crear_entrenadores/', views.formulario_entrenadores, name='FormularioEntrenadores'),
+    path('crear_torneos/', views.formulario_torneos, name='FormularioTorneos'),
+    path('buscar_jugador/', views.buscar_jugador, name='BuscarJugador'),
+    path('mostrar_jugadores/', views.mostrar_jugadores_todos, name='MostrarJugadores'),
+    path('eliminar_jugadores/<apellido>', views.eliminar_jugador, name='EliminarJugadores'),
+    path('actualizar_jugadores/<jugador_id>', views.actualizar_jugadores, name='ActualizarJugadores'),
+    path('jugadores_list/', views.JugadoresList.as_view(), name='List'),
+    path('jugadores_detail/<pk>', views.JugadoresDetailView.as_view(), name='Detail'),
+    path('jugadores_confirm_delete/<pk>', views.JugadoresDeleteView.as_view(), name='Delete'),
+    path('jugadores_edit/<pk>', views.JugadoresUpdateView.as_view(), name='Update'),
+    path('jugadores_create/', views.JugadoresCreateView.as_view(), name='Create'),
+    path('signup/', views.SignUpView.as_view(), name='Sign Up'),
+    path('login/', views.AdminLoginView.as_view(), name='Login'),
+    path('logout/', views.AdminLogoutView.as_view(), name='Logout'),
+    path('editar_usuario/', views.editar_usuario, name='Editar usuario'),
+    path('no_info/', views.no_hay_info, name='Nomoreinfo'),
+    path('crear_blogs/', views.formulario_blogs, name='FormularioBlogs'),
+]
